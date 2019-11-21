5b606b95cc25dd68a33ae2e86e6ee060a1e35d63
Aduniomaster6969 <54900317+Aduniomaster6969@users.noreply.github.com>
2019-11-21 09:48:10 -0500

    added

--- practise.js
+++ practise.js
@@ -1,8 +1,22 @@
 function A1() {
+	document.getElementById("helloo").innerHTML = "your doo doo"; 
 	alert("your children has been kidnapped")
 }
 
 function A2() {
+	document.getElementById("hellooo").innerHTML = "JOE MAMA"
 	let name= prompt("whats your name?")
-	alert(name+", your a peice of garbage ")
+	alert(name+", your a piece of garbage ")
 }
+
+ function myFunction() {
+    document.getElementById("helloooo").innerHTML = "KSI the Champ"; 
+    let array = ['Caillou','mr.Bean', 'Canned Coochie'];
+    alert(array[2]);
+        alert(array[0]);
+                alert(array[1]);
+  }
+
+
+    
+
